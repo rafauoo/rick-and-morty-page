61e5744e698661bcd8e9bba624bce60376d94ed9
rafal <rafal5094@gmail.com>
2023-02-12 16:03:45 +0100

    Add mobile version of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './Header.css';
 
+function HeaderText() {
+    return <text id="header-text">LOREM</text>
+}
+
 function Header() {
-    return (
-        <header id="header">
-            <text id="header-text">LOREM</text>
-            <div className="arrow-up"></div>
-            <div id="header-blue">
-            </div>
-        </header>
-    )
+        return (
+            <header id="header">
+                <HeaderText/>
+                <div className="arrow-up"></div>
+                <div id="header-blue">
+                </div>
+            </header>
+        )
 }
 
 export default Header;
