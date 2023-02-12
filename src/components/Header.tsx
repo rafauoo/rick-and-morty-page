f558cdc7e6645dce407cfb9e35bbbcc0990fa445
rafal <rafal5094@gmail.com>
2023-02-12 14:47:57 +0100

    Add header - desktop version

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+    return (
+        <header id="header">
+            <text id="header-text">LOREM</text>
+            <div className="arrow-up"></div>
+            <div id="header-blue">
+            </div>
+        </header>
+    )
+}
+
+export default Header;
