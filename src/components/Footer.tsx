29e6aff96a600477f7b55de722c4c51abcb77845
rafal <rafal5094@gmail.com>
2023-02-14 11:20:12 +0100

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+        return (
+            <footer id="footer">
+                <text id="footer-text">LOREM IPSUM ©2021</text>
+            </footer>
+        )
+}
+
+export default Footer;
