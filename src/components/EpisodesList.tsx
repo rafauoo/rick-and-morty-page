8c94d379991ec9d942c3810e1b9fbd7221e8e49e
rafal <rafal5094@gmail.com>
2023-02-14 14:02:43 +0100

    Add mobile version of episodes list

--- src/components/EpisodesList.tsx
+++ src/components/EpisodesList.tsx
@@ -14,12 +14,17 @@ function EpisodesList() {
     const air_dates_list = air_dates.map((number) =>
     <li className="air_date">{number}</li>
     );
+    const hl = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const hl_list = hl.map((number) =>
+    <li className="horizontal-line"></li>
+    );
         return (
             <div>
                 <ul id="episodes-list">{listItems}</ul>
                 <div id="vl"></div>
                 <ul id="names-list">{names_list}</ul>
                 <ul id="air-dates-list">{air_dates_list}</ul>
+                <ul id="hlines">{hl_list}</ul>
             </div>
         )
 }
