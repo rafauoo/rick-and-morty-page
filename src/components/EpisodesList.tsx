019f118c3352dd49b113ef7d99afa990e4b8296f
rafal <rafal5094@gmail.com>
2023-02-15 21:09:30 +0100

    Horizontal lines generate by episodes count
    Horizontal lines were generated by constant array that had 9 elements.
    Now they are generated dynamically by episodes count.
    (ex. 12 episodes -> 11 lines)

--- src/components/EpisodesList.tsx
+++ src/components/EpisodesList.tsx
@@ -29,19 +29,19 @@ function EpisodesList() {
     const air_dates_list = data?.episodes?.results?.map((result: any) =>
     <li className="air_date">{result.air_date}</li>
     );
-    const hl = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const hl_list = hl.map(() =>
-    <li className="horizontal-line"></li>
-    );
-        return (
-            <div>
-                <ul id="episodes-list">{listItems}</ul>
-                <div id="vl"></div>
-                <ul id="names-list">{names_list}</ul>
-                <ul id="air-dates-list">{air_dates_list}</ul>
-                <ul id="hlines">{hl_list}</ul>
-            </div>
-        )
+    const hlines: JSX.Element[] = [];
+    for (let i = 1; i <= data?.episodes?.results?.length - 1; i++) {
+        hlines.push(<li className="horizontal-line" key={i} />);
+    }
+    return (
+        <div>
+            <ul id="episodes-list">{listItems}</ul>
+            <div id="vl"></div>
+            <ul id="names-list">{names_list}</ul>
+            <ul id="air-dates-list">{air_dates_list}</ul>
+            <ul id="hlines">{hlines}</ul>
+        </div>
+    )
 }
 
 export default EpisodesList;
