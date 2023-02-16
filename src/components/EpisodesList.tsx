6b6ca20104b5cb9f62a640f7a4221041c99b0126
rafal <rafal5094@gmail.com>
2023-02-16 10:11:53 +0100

    Add keys to lists
    There was a warning showing that list elements should have a unique key
    
    Resolves: #6

--- src/components/EpisodesList.tsx
+++ src/components/EpisodesList.tsx
@@ -20,17 +20,17 @@ function EpisodesList() {
     console.log("loading", loading);
     console.log("error", error);
     console.log(data);
-    const listItems = data?.episodes?.results?.map((result: any) =>
-    <li className="episodes">{result.episode}</li>
+    const listItems = data?.episodes?.results?.map((result: any, index: number) =>
+    <li className="episodes" key={index}>{result.episode}</li>
     );
-    const names_list = data?.episodes?.results?.map((result: any) =>
-    <li className="name">{result.name}</li>
+    const names_list = data?.episodes?.results?.map((result: any, index: number) =>
+    <li className="name" key={index}>{result.name}</li>
     );
-    const air_dates_list = data?.episodes?.results?.map((result: any) =>
-    <li className="air_date">{result.air_date}</li>
+    const air_dates_list = data?.episodes?.results?.map((result: any, index: number) =>
+    <li className="air_date" key={index}>{result.air_date}</li>
     );
     const hlines: JSX.Element[] = [];
-    for (let i = 1; i <= data?.episodes?.results?.length - 1; i++) {
+    for (let i = 0; i < data?.episodes?.results?.length - 1; i++) {
         hlines.push(<li className="horizontal-line" key={i} />);
     }
     return (
