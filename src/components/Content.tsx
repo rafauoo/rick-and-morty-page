8c85910e2c69a21bfa21ec8fe1a3902805737c85
rafal <rafal5094@gmail.com>
2023-02-21 22:31:40 +0100

    Change position absolute to flexbox in list

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,14 +6,10 @@ import EpisodesList from "./EpisodesList";
 function Content() {
         return (
             <div id="content">
-                <div id="cover">
-                    <h1 id="h1-text">Episodes of the <span id="h1-4th">4th</span><br/>season 
-                    of the series<br/><span id="h1-rickandmorty">Rick and Morty</span></h1>
-                    <img src={image} alt="Rick And Morty" id="image"></img>
-                </div>
-                <div id="list">
-                    <EpisodesList />
-                </div>
+                <h1 id="h1-text">Episodes of the <span id="h1-4th">4th</span><br/>season 
+                of the series<br/><span id="h1-rickandmorty">Rick and Morty</span></h1>
+                <img src={image} alt="Rick And Morty" id="image"></img>
+                <EpisodesList />
             </div>
         )
 }
